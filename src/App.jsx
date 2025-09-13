2f8aa715c48e6c4db50a2527596c34c1e8f47dc0
Ali Hassan <alihere4587@gmail.com>
2025-09-13 21:41:07 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/coins/Coin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './coin.css' 
+
+function Coin() {
+  return (
+    <div>Coin</div>
+  )
+}
+
+export default Coin
